05c6e501dfeb9db32f010e50c5bf0f1ff8e2179c
Dok1709 <isx1709@yandex.ru>
2026-05-17 16:30:06 +0300

    first commit

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -19,19 +19,22 @@ const (
 func parsePackage(data string) (int, time.Duration, error) {
 	parts := strings.Split(data, ",")
 	if len(parts) != 2 {
-		return 0, 0, fmt.Errorf("Ошибка, колличество элементов не равно 2")
+		return 0, 0, fmt.Errorf("количество элементов не равно 2")
 	}
+
 	steps, err := strconv.Atoi(parts[0])
 	if err != nil {
 		return 0, 0, err
 	}
 	if steps <= 0 {
-		return 0, 0, fmt.Errorf("Колличество шагов должно быть больше нуля")
+		return 0, 0, fmt.Errorf("количество шагов должно быть больше нуля")
 	}
+
 	duration, err := time.ParseDuration(parts[1])
 	if err != nil {
 		return 0, 0, err
 	}
+
 	return steps, duration, nil
 }
 
@@ -44,13 +47,16 @@ func DayActionInfo(data string, weight, height float64) string {
 	if steps <= 0 {
 		return ""
 	}
+
 	distanceMeters := float64(steps) * stepLength
 	distanceKm := distanceMeters / float64(mInKm)
+
 	calories, calErr := spentcalories.WalkingSpentCalories(steps, weight, height, duration)
 	if calErr != nil {
 		fmt.Println(calErr)
 		return ""
 	}
+
 	return fmt.Sprintf(
 		"Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.",
 		steps, distanceKm, calories,
