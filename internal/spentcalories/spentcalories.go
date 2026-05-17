813f658caeef5731b66bedba1e5b6735d14afbbf
Dok1709 <isx1709@yandex.ru>
2026-05-17 17:40:24 +0300

    first commit

--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -80,7 +80,7 @@ func TrainingInfo(data string, weight, height float64) (string, error) {
 		return "", fmt.Errorf("неизвестный тип тренировки")
 	}
 	result := fmt.Sprintf(
-		"Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f",
+		"Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", // Добавлен \n в конец
 		trainingType, duration.Hours(), distanceKm, speedKmH, calories,
 	)
 	return result, nil
